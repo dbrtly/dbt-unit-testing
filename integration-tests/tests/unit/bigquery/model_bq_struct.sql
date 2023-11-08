46c42b9b6922d53835a21df3308b71c75e3c74ed
Daniel Bartley <dbrtly00@.gmail.com>
2023-11-08 14:20:39 +1100

    fix: mock reference corrected

--- integration-tests/tests/unit/bigquery/model_bq_struct.sql
+++ integration-tests/tests/unit/bigquery/model_bq_struct.sql
@@ -5,7 +5,7 @@
 }}
 
 {% call dbt_unit_testing.test('model_bq_struct', 'should be return a struct') %}
-  {% call dbt_unit_testing.mock_ref ('model_bq_struct') %}
+  {% call dbt_unit_testing.mock_ref ('model_bq_struct_stub') %}
     select 1 as a, struct('b' as b, true as c) as my_struct
   {% endcall %}
   {% call dbt_unit_testing.expect() %}
